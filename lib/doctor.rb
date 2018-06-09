34bd0cfdb90069f22cb14c66fadfa7bb36764ec9
Monica Marziano <mmarziano1@gmail.com>
2018-06-09 01:43:11 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,31 @@
+class Doctor
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end 
+  
+  def appointments
+    Appointment.all.select do |doctor|
+      doctor.doctor == self
+    end
+  end
+  
+  def patients
+    self.appointments.collect do |doctor|
+      doctor.patient
+    end
+  end
+  
+end 
---
+++ lib/patient.rb
@@ -0,0 +1,28 @@
+class Patient 
+  attr_accessor :name, :date, :doctor 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end 
+  
+  def appointments
+    Appointment.all.select do |patient|
+      patient.patient == self
+    end
+  end 
+  
+  def doctors
+    
+  end
+  
+end 
